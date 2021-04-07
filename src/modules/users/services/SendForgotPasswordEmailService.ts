e94ade2d380aec8b50d6f02095e88f472fc4554f
simaraconceicao <contato@simaraconceicao.com>
2021-04-07 16:45:40 -0300

    inicia feature de recuperação de senha com TDD

---
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -0,0 +1,45 @@
+import { injectable, inject } from 'tsyringe';
+
+import AppError from '@shared/errors/AppError'
+import IUsersRepository from '../repositories/IUsersRepository';
+import IMailProvider from '@shared/container/providers/MailProvider/Models/IMailProvider';
+import IUserTokensRepository from '../repositories/IUserTokensRepository';
+
+// import User from '../infra/typeorm/entities/User'
+
+interface IRequest {
+    email: string
+}
+
+@injectable()
+class SendForgotPasswordEmailService {
+    constructor(
+        @inject('UsersRepository')
+        private usersRepository: IUsersRepository,
+
+        @inject('MailProvider')
+        private mailProvider: IMailProvider,
+
+        @inject('UserTokensRepository')
+        private userTokensRepository: IUserTokensRepository,
+    ){};
+
+    public async execute({ email }: IRequest): Promise<void> {
+        const user = await this.usersRepository.findByEmail(email);
+
+        if(!user){
+            throw new AppError('User does not exists');
+        }
+
+        await this.userTokensRepository.generate(user.id);
+
+        this.mailProvider.sendMail(
+            email,
+            'pedido de recuperacao de senha recebido',
+        )
+    }
+
+}
+
+
+export default SendForgotPasswordEmailService;
