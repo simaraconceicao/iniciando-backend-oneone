2eeb71b6a1d3af810a4033e710725a420610deb1
simaraconceicao <contato@simaraconceicao.com>
2021-04-07 19:48:19 -0300

    reset de senha com TDD

---
+++ src/modules/users/services/ResetPasswordService.spec.ts
@@ -0,0 +1,44 @@
+import 'reflect-metadata';
+
+import AppError from '@shared/errors/AppError';
+
+import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
+import ResetPasswordService from './ResetPasswordService';
+import FakeUserTokensRepository from '../repositories/fakes/FakeUserTokensRepository';
+
+let fakeUsersRepository: FakeUsersRepository;
+let fakeUserTokensRepository: FakeUserTokensRepository;
+let resetPassword: ResetPasswordService;
+
+describe('SendForgotPasswordEmail', () => {
+    beforeEach(() => {
+        fakeUsersRepository = new FakeUsersRepository();
+        fakeUserTokensRepository =  new FakeUserTokensRepository();
+
+        resetPassword = new ResetPasswordService(
+            fakeUsersRepository,
+            fakeUserTokensRepository,
+        );
+    });
+
+    it('should be able to reset the password', async () => {        
+        const user = await fakeUsersRepository.create({
+            name: 'John Doe',
+            email: 'johndoe@email.com',
+            password: '123456'
+        })
+
+        const { token } = await fakeUserTokensRepository.generate(user.id);
+
+        await resetPassword.execute({
+            password: 'johndoe',
+            token,
+        })
+
+       const updatedUser = await fakeUsersRepository.findById(user.id)
+
+        expect(updatedUser?.password).toBe('johndoe');
+    });
+
+    
+})
